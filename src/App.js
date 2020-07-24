74e8c98f8efa3c7082a61515cef10bc2542a1812
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-07-24 14:52:53 +0200

    Implement Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
+import LevelSelection from './pages/LevelSelection';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 
 function App() {
   return (
-    <div>
-      <HomePage/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'><HomePage/></Route>
+        <Route path='/level-selection'><LevelSelection/></Route>
+      </Switch>
+    </Router>
   );
 }
 
