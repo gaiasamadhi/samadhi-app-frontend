511a17aaa82b7692c47d92882d356e521b11f623
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-07-25 19:01:17 +0200

    Implement sticky button

--- src/components/BeginnersCourseInfoIntroduction.js
+++ src/components/BeginnersCourseInfoIntroduction.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 const BeginnersCourseInfoIntroduction = (props) => {
   const {banner,description, dayOfUpdate, monthOfUpdate, yearOfUpdate, suitableFor} = props;
@@ -15,6 +16,9 @@ const BeginnersCourseInfoIntroduction = (props) => {
         <div className='row justify-content-center'>
           <div className="col-10 my-2">
             {description}
+          </div>
+          <div className="fixed-top offset-8 my-0">
+            <button type="button" className="btn btn-info btn-lg btn-block" style={{marginTop: '200px'}}><Link className="text-white" to="beginners-course-content">Go to course</Link></button>
           </div>
         </div>
         <div className='row '>
