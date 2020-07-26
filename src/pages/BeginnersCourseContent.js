8f9ca39226460e545498763053783bab3c28f088
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-07-26 19:08:23 +0200

    Update page

--- src/pages/BeginnersCourseContent.js
+++ src/pages/BeginnersCourseContent.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import PageHeader from '../components/PageHeader';
+import MediaPlayer from '../components/MediaPlayer';
+import BeginnersCourseTabsWithCourseContent from '../components/BeginnersCourseTabsWithCourseContent';
 
 const BeginnersCourseContent = () => {
   return(
-    null
+    <div className="beginners-course-content">
+      <PageHeader
+      title={'Meditation for beginners: a practical guide'}
+      />
+      <MediaPlayer/>
+      <BeginnersCourseTabsWithCourseContent/>
+    </div>
   )
-}
+};
 
 export default BeginnersCourseContent;
