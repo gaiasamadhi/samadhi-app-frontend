4dd49402a39767f1c0102b062002cce1129848cd
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-07-26 19:03:56 +0200

    Implement ModuleTabs component

---
+++ src/components/ModuleTabs.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link,  useRouteMatch} from 'react-router-dom';
+
+const ModuleTabs = (props) => {
+  const {courseContent, questionsAndAnswers} =props;
+  const match = useRouteMatch();
+
+  return (
+    <div className="module-tabs">
+      <ul className="nav nav-pills nav-fill">
+        <li className="nav-item">
+          <a className= {"nav-link " + courseContent}  href=""><Link className="text-dark" to="/beginners-course-content">course content</Link></a>
+        </li>
+        <li className="nav-item">
+          <a className= {"nav-link " + questionsAndAnswers} href=""><Link className="text-dark" to={match.url + "/browse-questions"}>questions and answers</Link></a>
+        </li>
+      </ul>
+      {console.log(match.url - "/browse")}
+    </div>
+  )
+}
+
+export default ModuleTabs;
