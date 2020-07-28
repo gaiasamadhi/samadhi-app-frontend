58e06801bf7c21843fc46292e9a24f7a8dd024f4
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-07-28 14:48:55 +0200

    Implement BrowseQuestionsContainer component

---
+++ src/containers/BrowseQuestionsContainer.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+
+import BrowseQuestions from '../components/BrowseQuestions';
+
+
+const BrowseQuestionsContainer = () => {
+  const [questions, updateQuestions] = useState ([])
+  const [newQuestionValue,updateNewQuestionValue] = useState("");
+
+  const [fetchStatus, setFetchStatus] = useState('IDLE'); // STARTED, SUCCEED, FAILED
+  const [errMessage, setErrMessage] = useState(null);
+  const [listQuestions, setListQuestions] = useState([]);
+
+  const newQuestionValueOnChangeHandler = event => {
+    const val = event.target.value
+
+      updateNewQuestionValue(val)
+
+  };
+
+  const onSearchQuestionClickHandler = () => {
+        setFetchStatus('STARTED')
+
+        fetch(`http://localhost:1337/questions/?description_contains=${newQuestionValue}`)
+                .then(response => response.json())
+                .then(data => {
+                    setListQuestions(data);
+                    const array = data.map((filteredQuestion) => (filteredQuestion.description))
+                    //const searchQuery = array.map((field) =>field.search(/newQuestionValue/i))
+
+                    updateQuestions(array);
+                    updateNewQuestionValue("");
+                    setFetchStatus('SUCCEED');
+
+
+                })
+                .catch(err => {
+                    setErrMessage(err.message);
+                    setFetchStatus('FAILED');
+                })
+  }
+
+  return (
+    <div>
+      <BrowseQuestions
+      questions = {questions}
+      newQuestionValue= {newQuestionValue}
+      newQuestionValueOnChangeHandler= {newQuestionValueOnChangeHandler}
+      onSearchQuestionClickHandler= {onSearchQuestionClickHandler}
+      filteredListQuestions = {listQuestions}
+      />
+    {/*console.log(questions)*/}
+    </div>
+  )
+}
+
+export default BrowseQuestionsContainer;
