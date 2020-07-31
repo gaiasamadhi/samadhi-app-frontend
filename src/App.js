eb4e4f6ef1bd6039b8dd0bbcfa28593c630821ac
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-07-31 16:50:02 +0200

    Add new Routes

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import BeginnersCourseInfo from './pages/BeginnersCourseInfo';
 import BeginnersCourseContent from './pages/BeginnersCourseContent';
 import BeginnersBrowseQuestions from './pages/BeginnersBrowseQuestions';
 import BeginnersSubmitAnswer from './pages/BeginnersSubmitAnswer';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import BeginnersPostQuestion from './pages/BeginnersPostQuestion';
+import {BrowserRouter as Router, Switch, Route, useRouteMatch} from 'react-router-dom';
 
 import './App.css';
 
 
 function App() {
+  //const {path, url} = useRouteMatch()
+
   return (
     <Router>
       <Switch>
         <Route exact path='/'><HomePage/></Route>
         <Route path='/level-selection'><LevelSelection/></Route>
-        <Route path='/beginners-course-info' ><BeginnersCourseInfo/></Route>
+        <Route path='/beginners-course-info'><BeginnersCourseInfo/></Route>
+        <Route path='/beginners-course-content/:browse/ask'><BeginnersPostQuestion/></Route>
         <Route path='/beginners-course-content/:browse/:id'><BeginnersSubmitAnswer/></Route>
         <Route path='/beginners-course-content/:browse'><BeginnersBrowseQuestions/></Route>
-
+        {/*<Route path='/beginners-course-content/:browse/:id'><BeginnersSubmitAnswer/></Route>
+        //<Route path='/beginners-course-content/:browse/:ask'><BeginnersPostQuestion/></Route>
+        */}
         <Route path='/beginners-course-content'><BeginnersCourseContent/></Route>
+
       </Switch>
     </Router>
   );
