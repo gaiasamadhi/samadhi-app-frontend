625a91e79c08053c7bfc9d86e04f051ab3345359
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-08-02 16:48:01 +0200

    Add image from backend

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -3,14 +3,17 @@ import PropTypes from 'prop-types';
 import logo from '../assets/github_icon.png'
 
 const PageHeader = (props) => {
-  const {title} = props;
+  const {title, imageList} = props;
+
+  console.log(imageList[0] && imageList[0]["images"].map(text=>text.name))
+  const newLogo = imageList[0] && imageList[0]["images"].map(text=>text.url)
 
   return (
   <div className='page-header'>
-          <div className="d-flex justify-content-around bg-info h3 mb-0">
+          <div className="d-flex justify-content-around h3 mb-0">
             <div className="p-3 bd-highlight">
               <span>Samadhi </span>
-              <img src={logo} width="30" height="30" className="" alt="" loading="lazy"/>
+              <img src={newLogo} width="30" height="30" className="" alt="" loading="lazy"/>
             </div>
             <span className="p-3 bd-highlight">{title}</span>
             <span className="p-3 bd-highlight">EN</span>
