339892ab2e58f4d8e742b0addd4415b637292efa
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-08-03 18:02:58 +0200

    Update content

--- src/components/BeginnersCourseInfoGoals.js
+++ src/components/BeginnersCourseInfoGoals.js
@@ -14,18 +14,18 @@ const BeginnersCourseInfoGoals = (props) => {
         </div>
           <div className="row ">
             <ul className =" list-group col-3 offset-1 mb-5">
-              <li className="list-group-item list-group-item-success">Value 1</li>
-              <li className="list-group-item list-group-item-success">Value 2
+              <li className="list-group-item list-group-item-success">Increase focus</li>
+              <li className="list-group-item list-group-item-success">Achieve deeper sense of relaxation
               </li>
-              <li className="list-group-item list-group-item-success">Value 3
+              <li className="list-group-item list-group-item-success">Get in balance with the environment
               </li>
             </ul>
             <ul className ="list-group col-3 offset-0 mb-5">
-              <li className="list-group-item list-group-item-success">Value 4
+              <li className="list-group-item list-group-item-success">Improves self-esteem
               </li>
-              <li className="list-group-item list-group-item-success">Value 5
+              <li className="list-group-item list-group-item-success">Decrease undesired emotions
               </li>
-              <li className="list-group-item list-group-item-success">Value 6
+              <li className="list-group-item list-group-item-success">Promotes feelings of joy
               </li>
             </ul>
           </div>
