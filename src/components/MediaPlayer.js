08eeb04d86aa63a9e4bf56f745fb9e710b087c74
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-08-04 16:45:19 +0200

    Update opacity plus background color

--- src/components/MediaPlayer.js
+++ src/components/MediaPlayer.js
@@ -3,23 +3,24 @@ import React from 'react';
 const MediaPlayer = () => {
   return (
     <div className="media-player">
-      <div className ="container-fluid bg-warning">
-        <div className ="row justify-content-start">
-          <div className ="col-4 my-3">
+      <div className ="container-fluid bg" style = {{backgroundColor: "#5b56bc", opacity: "0.8"}}>
+        <div className ="row justify-content-center py-3">
+          <div className ="col-6 my-3 text-white">
             <div className ="row h5 justify-content-center">
-            welcome :-)
+            Welcome :-)
             </div>
-            <div className ="row justify-content-center">
+            <div className ="row justify-content-center py-3">
             to get started just click on the play button on the media player
             </div>
-          </div>
-          <div className ="col-4 my-3 embed-responsive embed-responsive-16by9">
-            <iframe
-              className="embed-responsive-item"
-              src="https://www.youtube.com/embed/5GSeWdjyr1c"
-              frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen="allowfullscreen"></iframe>
+
+            <div className ="row justify-content-center my-3 embed-responsive embed-responsive-16by9">
+              <iframe
+                className="embed-responsive-item"
+                src="https://www.youtube.com/embed/5GSeWdjyr1c"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen="allowfullscreen"></iframe>
+            </div>
           </div>
         </div>
     </div>
