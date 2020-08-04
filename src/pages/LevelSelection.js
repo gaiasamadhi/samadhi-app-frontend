f39b427a98aa741fff29f2158b0f817139a7b10a
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-08-04 16:46:07 +0200

    Update background image plus footer prop

--- src/pages/LevelSelection.js
+++ src/pages/LevelSelection.js
@@ -7,12 +7,16 @@ import PageFooter from '../components/PageFooter';
 const LevelSelection = () => {
   return (
         <div className='level-selection'>
-          <PageHeader title={''}/>
+          <PageHeader
+            title={''}
+            backgroundImage = {"https://res.cloudinary.com/dr33x9zki/image/upload/v1596394226/Home_Content_Background_3fdffdceb9.jpg"}/>
           <LevelSelectionContent
-            banner={'please choose one of the following paths'}
+            banner={'Please choose one of the following paths'}
             description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-            />
-          <PageFooter footer={'License GPL Version 2'}/>
+            backgroundImage = {"https://res.cloudinary.com/dr33x9zki/image/upload/v1596396986/Home_Content_Background_00_d5bdb382dd.jpg"}/>
+          {/*https://res.cloudinary.com/dr33x9zki/image/upload/v1596458186/lotus_Background_679344e864.jpg*/}
+          <PageFooter
+            footer={'GNU General Public License version 2'}/>
         </div>
   )
 }
