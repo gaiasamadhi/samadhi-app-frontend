7348fdb77dbb6419fcb9b2d36b248e5497d12b9c
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-08-04 13:56:59 +0200

    Update backgroundImage

--- src/components/BeginnersCourseInfoIntroduction.js
+++ src/components/BeginnersCourseInfoIntroduction.js
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
 const BeginnersCourseInfoIntroduction = (props) => {
-  const {banner,description, dayOfUpdate, monthOfUpdate, yearOfUpdate, suitableFor} = props;
+  const {banner,description, dayOfUpdate, monthOfUpdate, yearOfUpdate, suitableFor, backgroundImage} = props;
 
   return(
     <div className='beginners-course-info-introduction'>
-      <div className='container-fluid bg-warning'>
+      <div className='container-fluid text-white' style = {{backgroundImage: `url(${backgroundImage})`,
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      opacity: "1"}}
+      > {/*style = {{backgroundColor: "#b987f1", opacity: "0.9"}}*/}
         <div className='row justify-content-center'>
           <div className="col-10 h4 my-5">
             {banner}
